73a0b90a0cea957b5410b80226256ff2a3479ac0
Pareesa Madjd <pareesa.madjd@gmail.com>
2021-09-17 15:50:45 -0400

    Martian Weight pair exercise is complete.

--- exercise-pair/java/src/main/java/com/techelevator/MartianWeight.java
+++ exercise-pair/java/src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  In case you've ever pondered how much you weight on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -19,6 +21,21 @@ Enter a series of Earth weights (space-separated): 98 235 185
 public class MartianWeight {
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter a series of Earth weights (space-separated):");
+		String strEarthWeights = scanner.nextLine();
+		String [] earthWeightsArray = strEarthWeights.split(" ");
+
+		System.out.println("Your new Mars weights are as follows:");
+
+		for (int i=0; i < earthWeightsArray.length; i++){
+			String weight = earthWeightsArray[i];
+			int eWeight = Integer.parseInt(weight);
+			int marsWeight = (int)(eWeight * 0.378);
+			System.out.println(eWeight + " lbs. on Earth, is " + marsWeight + " lbs. on Mars.");
+		}
 
 	}
 
